3361bcd63342666aae6b26e7be2572af69d69e24
施颖杰 <1577310596@qq.com>
2022-07-28 22:35:52 +0800

    feat: finish second test-cases

--- src/views/Header/Header.vue
+++ src/views/Header/Header.vue
@@ -1,9 +1,10 @@
 <template>
-  <input data-test="input" />
+  <input data-test="input" :value="inputValue" />
 </template>
 
 <script setup lang='ts'>
-
+import { ref } from 'vue'
+const inputValue = ref<string>('')
 </script>
 
 <style>
--- src/views/Header/__tests__/unit/Header.spec.ts
+++ src/views/Header/__tests__/unit/Header.spec.ts
@@ -1,9 +1,29 @@
 import { shallowMount } from '@vue/test-utils'
 import Header from '@/views/Header/Header.vue'
+
+let wrapper: any = null
+beforeAll(() => {
+  wrapper = shallowMount(Header)
+})
 describe('input', () => {
+  // 首先确保有这个 input 元素
   it('we should have a input DOM', () => {
-    const wrapper = shallowMount(Header)
     const input = wrapper.find('[data-test="input"]')
     expect(input.exists()).toBe(true)
+  })
+
+  // 获取到 input 的 value 值
+  it('get input value', () => {
+    // 通过 DOM 获取的值
+    const value = wrapper.get('input').wrapperElement.value
+
+    console.log('控制台输出：', wrapper.vm.$data)
+
+    // console.log('控制台输出：', value)
+
+    expect(value).toBe('')
+  })
+  // 当用户输入值后 input 的 value 值也应当同时改变
+  it.skip("input's value should be changed when user input the value", () => {
   })
 })
