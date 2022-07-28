d72d310efe62b0f48923d69ce2e4a71efced2f3e
施颖杰 <1577310596@qq.com>
2022-07-28 21:19:32 +0800

    chore: init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <h1>Home</h1>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script setup lang='ts'>
 
-@Options({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
 </script>
+
+<style>
+</style>
