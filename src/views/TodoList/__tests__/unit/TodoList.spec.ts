6e4aae92b10547b22139659559cc87181491c879
施颖杰 <1577310596@qq.com>
2022-07-29 17:52:27 +0800

    feat: finish third cases

--- src/views/TodoList/__tests__/unit/TodoList.spec.ts
+++ src/views/TodoList/__tests__/unit/TodoList.spec.ts
@@ -4,9 +4,12 @@ import TodoList from '@/views/TodoList/TodoList.vue'
 
 let wrapper: any = null
 let todoList: any = null
+let setupState: Record<string, any> = {}
+
 beforeAll(() => {
   wrapper = shallowMount(TodoList)
   todoList = wrapper.find('[data-test="todoList"]')
+  setupState = wrapper.vm.$.setupState
 })
 
 describe('TodoList test', () => {
@@ -16,5 +19,9 @@ describe('TodoList test', () => {
   it('we should have no element before user input', () => {
     const el = wrapper.get('ul').wrapperElement
     expect(el.children.length).toBe(0)
+  })
+  it('we should have a reactive variable', () => {
+    const { undoList } = setupState
+    expect(undoList).toEqual([])
   })
 })
