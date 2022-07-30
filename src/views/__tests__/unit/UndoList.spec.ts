1541f9c9e880a605d8c2cd5642345147991203d8
施颖杰 <1577310596@qq.com>
2022-07-30 22:39:21 +0800

    feat: add third cases for undoList

--- src/views/__tests__/unit/UndoList.spec.ts
+++ src/views/__tests__/unit/UndoList.spec.ts
@@ -1,5 +1,6 @@
-import UndoList from '@/views/UndoList.vue'
 import { mount } from '@vue/test-utils'
+import { findTestWrapper } from '@/utils'
+import UndoList from '@/views/UndoList.vue'
 
 describe('UndoList', () => {
   it('UndoList args should be [], count shoule be 0, list length should be 0 too', () => {
@@ -15,5 +16,37 @@ describe('UndoList', () => {
     expect(wrapper.html()).toContain([])
     expect(Number.parseInt(countElem[0].text())).toBe(0)
     expect(listItems.length).toBe(0)
+  })
+  it('UndoList 参数为[1, 2, 3], count 的值应该为 3，且列表有内容', () => {
+    const wrapper = mount(UndoList, {
+      props: {
+        undoList: [1, 2, 3]
+      }
+    })
+
+    const countElem = findTestWrapper(wrapper, 'count')
+    const listItems = findTestWrapper(wrapper, 'list')
+
+    expect(wrapper.html()).toContain([])
+    expect(Number.parseInt(countElem[0].text())).toBe(3)
+    expect(listItems.length).toBe(3)
+  })
+  it('UndoList 参数为[1, 2, 3], count 的值应该为 3，且列表有内容, 且存在删除按钮', () => {
+    const wrapper = mount(UndoList, {
+      props: {
+        undoList: [1, 2, 3]
+      }
+    })
+
+    const countElem = findTestWrapper(wrapper, 'count')
+    const listItems = findTestWrapper(wrapper, 'list')
+
+    // add
+    const deleteBtns = findTestWrapper(wrapper, 'delete-button')
+
+    expect(wrapper.html()).toContain([])
+    expect(Number.parseInt(countElem[0].text())).toBe(3)
+    expect(listItems.length).toBe(3)
+    expect(deleteBtns.length).toBe(3)
   })
 })
