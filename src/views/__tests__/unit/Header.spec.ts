485765eb748098307dcda3b40a4a7b194d5801ec
凤之兮原 <1577310596@qq.com>
2022-08-04 21:32:16 +0800

    change: change type to WrapperType

--- src/views/__tests__/unit/Header.spec.ts
+++ src/views/__tests__/unit/Header.spec.ts
@@ -1,8 +1,8 @@
-import { mount } from '@vue/test-utils'
+import { VueWrapper, DOMWrapper, mount } from '@vue/test-utils'
 import Header from '@/views/Header.vue'
 
-let wrapper: any = null
-let input: any = null
+let wrapper: VueWrapper<any>
+let input: DOMWrapper<any>
 
 beforeAll(() => {
   wrapper = mount(Header)
@@ -17,16 +17,14 @@ describe('notice', () => {
 })
 
 describe('input', () => {
-  // 首先确保有这个 input 元素
   it('we should have a input Element', () => {
-    console.log(Header.render)
     expect(input.exists()).toBe(true)
   })
 
   // 获取到 input 的 value 值
   it('get input value', () => {
     // 通过 DOM 获取的值
-    const value = wrapper.get('input').wrapperElement.value
+    const value = wrapper.get('input').element.value
     expect(value).toBe('')
   })
 
@@ -35,7 +33,7 @@ describe('input', () => {
     // 假设用户输入了一串值
     input.setValue('this is test value')
     // 获取到用户输入的数据
-    const value = wrapper.get('input').wrapperElement.value
+    const value = wrapper.get('input').element.value
     expect(value).toBe('this is test value')
   })
 
@@ -50,11 +48,11 @@ describe('input', () => {
 
   // 如果输入有内容，并且输入回车，则触发 emit 事件，同时清空 inputValue
   it('we should trigger emit and then clear inputValue when user input key is pressed', () => {
-    const value = wrapper.get('input').wrapperElement.value
+    const value = wrapper.get('input').element.value
     input.setValue('anything')
     input.trigger('keyup.enter')
     expect(wrapper.emitted().add).toBeTruthy()
-    expect(wrapper.emitted('add')[0]).toEqual(['anything'])
+    expect((wrapper.emitted('add') as any)[0]).toEqual(['anything'])
     expect(value).toBe('')
   })
 })
