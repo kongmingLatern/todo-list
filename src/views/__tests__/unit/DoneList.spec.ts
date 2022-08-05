253caa5009cce7bff12f3f317d51fd479522b25c
凤之兮原 <1577310596@qq.com>
2022-08-05 16:39:30 +0800

    feat: add cases for DoneList

--- src/views/__tests__/unit/DoneList.spec.ts
+++ src/views/__tests__/unit/DoneList.spec.ts
@@ -1,10 +1,14 @@
 import DoneList from '@/views/DoneList.vue'
-import { shallowMount, VueWrapper } from '@vue/test-utils'
+import { mount, VueWrapper } from '@vue/test-utils'
 import { findTestWrapper } from '../../../utils/index'
 
 let wrapper: VueWrapper<any>
-beforeAll(() => {
-  wrapper = shallowMount(DoneList)
+beforeEach(() => {
+  wrapper = mount(DoneList, {
+    props: {
+      doneList: []
+    }
+  })
 })
 describe('DoneList', () => {
   it('we should have a list', () => {
@@ -18,5 +22,53 @@ describe('DoneList', () => {
     expect(wrapper.html()).toContain([])
     expect(Number.parseInt(countElem[0].text())).toBe(0)
     expect(listItems.length).toBe(0)
+  })
+  it('DoneList 参数为[{...}], count 的值应该为 3，且列表有内容', () => {
+    wrapper = mount(DoneList as any, {
+      props: {
+        doneList: [
+          {
+            value: '123'
+          },
+          {
+            value: '456'
+          },
+          {
+            value: '789'
+          }
+        ]
+      }
+    })
+
+    const countElem = findTestWrapper(wrapper, 'count')
+    const listItems = findTestWrapper(wrapper, 'list')
+
+    expect(wrapper.html()).toContain([])
+    expect(Number.parseInt(countElem[0].text())).toBe(3)
+    expect(listItems.length).toBe(3)
+  })
+  it('DoneList 参数为[{...}], count 的值应该为 3，且列表有内容, 且存在删除按钮', () => {
+    wrapper = mount(DoneList as any, {
+      props: {
+        doneList: [{
+          value: '123'
+        }, {
+          value: '456'
+        }, {
+          value: '567'
+        }]
+      }
+    })
+
+    const countElem = findTestWrapper(wrapper, 'count')
+    const listItems = findTestWrapper(wrapper, 'list')
+
+    // add
+    const deleteBtns = findTestWrapper(wrapper, 'delete-button')
+
+    expect(wrapper.html()).toContain([])
+    expect(Number.parseInt(countElem[0].text())).toBe(3)
+    expect(listItems.length).toBe(3)
+    expect(deleteBtns.length).toBe(3)
   })
 })
