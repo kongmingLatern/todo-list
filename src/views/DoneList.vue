186e0eb5ea1f23eee73485468caaeab0b2282ef1
凤之兮原 <1577310596@qq.com>
2022-08-05 14:36:18 +0800

    feat: add DoneList Component

---
+++ src/views/DoneList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    已做
+    <span>1</span>
+    件
+  </div>
+  <ul
+  data-test="ul"
+  >
+
+  </ul>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style>
+
+</style>
