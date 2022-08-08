1dd7207de040a4cdd7b43f3f59277ce4a3146420
凤之兮原 <1577310596@qq.com>
2022-08-08 16:18:09 +0800

    feat: add a test case for BDD

---
+++ src/views/__tests__/unit/intergration/TodoList.spec.ts
@@ -0,0 +1,34 @@
+import { findTestWrapper } from '@/utils'
+import TodoList from '@/views/TodoList.vue'
+import UndoList from '@/views/UndoList.vue'
+import { mount } from '@vue/test-utils'
+
+it(`
+  1. 用户会在 Header 输入框输入内容
+  2. 用户会点击回车按钮
+  3. 列表项应该增加用户输入内容的列表项
+`, () => {
+  const wrapper = mount(TodoList)
+  const inputElem = findTestWrapper(wrapper, 'header-input').at(0)
+
+  const content = 'something'
+  inputElem.setValue(content)
+  inputElem.trigger('change')
+  inputElem.trigger('keyup.enter')
+
+  const { undoList } = wrapper.vm.$.setupState
+
+  const wrap = mount(UndoList as any, {
+    props: {
+      undoList
+    }
+  })
+  console.log(wrap.vm.$.props)
+
+  // const { undoList: list} =  wrap.vm.$.props
+
+  const listItems = findTestWrapper(wrapper, 'list-item')
+
+  expect(listItems.length).toBe(1)
+  expect(listItems.at(0).text()).toContain('')
+})
